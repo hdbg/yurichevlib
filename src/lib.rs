08b5622f9ef5b9ce100150c4f075c06fd46070d0
Stanislav <90869971+hdbg@users.noreply.github.com>
2024-04-02 18:40:40 +0200

    [Proxy]: Added FromStr impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use colored::{Color, Colorize};
 use rand::{seq::SliceRandom, thread_rng, Rng};
 
@@ -12,6 +14,33 @@ pub enum ProxyKind {
     Http,
     Https,
 }
+#[cfg(feature = "proxy_socket")]
+impl FromStr for ProxyKind {
+    type Err = ProxyParseError;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input.to_ascii_lowercase().as_str() {
+            "socks5" => Ok(Self::Socks5),
+            "socks4" => Ok(Self::Socks4),
+            "http" => Ok(Self::Http),
+            "https" => Ok(Self::Https),
+
+            _ => Err(InvalidProxyKindSnafu.build()),
+        }
+    }
+}
+
+#[cfg(feature = "proxy_socket")]
+#[derive(Debug, snafu::Snafu)]
+pub enum ProxyParseError {
+    #[snafu(display("Verify correctness on proxy parts"))]
+    InvalidChunkCount,
+
+    #[snafu(display("Failed to parse port"))]
+    InvalidPort,
+
+    #[snafu(display("Failed to recognize proxy kind"))]
+    InvalidProxyKind,
+}
 
 #[cfg(feature = "proxy_socket")]
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
@@ -20,6 +49,44 @@ pub struct Proxy {
     pub addr: String,
     pub port: u16,
     pub creds: Option<(String, String)>,
+}
+
+#[cfg(feature = "proxy_socket")]
+impl FromStr for Proxy {
+    type Err = ProxyParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut input = input.split(":").collect::<Vec<_>>();
+
+        if input.len() != 3 && input.len() != 5 {
+            return Err(InvalidChunkCountSnafu.build());
+        }
+
+        let kind = ProxyKind::from_str(&input.remove(0))?;
+        let addr = input.remove(0).to_string();
+        let port: u16 = input
+            .remove(0)
+            .parse()
+            .map_err(|_| InvalidProxyKindSnafu.build())?;
+
+        let creds = {
+            if input.is_empty() {
+                None
+            } else {
+                let login = input.remove(0);
+                let password = input.remove(0);
+
+                Some((login.to_owned(), password.to_owned()))
+            }
+        };
+
+        Ok(Self {
+            kind,
+            addr,
+            port,
+            creds,
+        })
+    }
 }
 #[cfg(feature = "proxy_socket")]
 pub mod proxy_socket;
